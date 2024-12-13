717c05ebe0200cf409f967109f60367c47db0a1f
Acelle Krislette Rosales <acellekrislette@gmail.com>
2024-12-13 12:28:32 +0800

    Fixed file tree, followed code convention, & removed poweful object

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-#References
+# References
 @onready var item_drop = load("res://scenes/item_drop.tscn")
 @onready var human_spr = $human_spr
 @onready var item_spr = $item_spr
 
-#Variables----------------------------
+# Variables
 var speed = 100
 var movement_velocity = Vector2.ZERO  # Stores current velocity
 var dir = Vector2.DOWN # Tracks the last movement direction
@@ -14,28 +14,30 @@ var carrying_item = false
 var drop_pos: Vector2
 var items_in_range: Array = []
 
-var male_arms_down_frames = preload("res://male_arms_down.tres")
-var male_arms_up_frames = preload("res://male_arms_up.tres")
-var female_arms_down_frames = preload("res://female_arms_down.tres")
-var female_arms_up_frames = preload("res://female_arms_down.tres")
+var male_arms_down_frames = preload("res://frames/male_arms_down.tres")
+var male_arms_up_frames = preload("res://frames/male_arms_up.tres")
+var female_arms_down_frames = preload("res://frames/female_arms_down.tres")
+var female_arms_up_frames = preload("res://frames/female_arms_down.tres")
 
 enum Gender { MALE, FEMALE}
 var curr_gender = Gender.MALE
+
+# Enum to define states
+enum States { IDLE, MOVE }
+var current_state = States.IDLE
 
 
 func _ready():
 	item_spr.hide()
 	_update_spritesheet()
 
-# Enum to define states
-enum States { IDLE, MOVE }
-var current_state = States.IDLE
-
+	
 func _physics_process(delta):
 	handle_state_transitions()
-	perform_state_actions(delta)
-	velocity = movement_velocity  # Assign movement_velocity to the built-in velocity
-	move_and_slide()  # Move using the built-in velocity
+	perform_state_actions()
+	velocity = movement_velocity
+	move_and_slide()
+
 
 func handle_state_transitions():
 	if Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
@@ -43,7 +45,8 @@ func handle_state_transitions():
 	else:
 		current_state = States.IDLE
 
-func perform_state_actions(delta):
+
+func perform_state_actions():
 	match current_state:
 		States.MOVE:
 			var horizontal_input = Input.get_axis("left", "right")
@@ -75,7 +78,6 @@ func perform_state_actions(delta):
 
 			# Calculate movement velocity
 			movement_velocity = dir * speed
-
 		States.IDLE:
 			# Stop the character immediately
 			movement_velocity = Vector2.ZERO
@@ -90,18 +92,18 @@ func perform_state_actions(delta):
 			elif dir.x < 0:
 				$human_spr.play("w-idle")
 
+
 func _update_spritesheet():
-	
 	if curr_gender == Gender.MALE:
 		if carrying_item == true:
-			$human_spr.frames = male_arms_up_frames
+			human_spr.frames = male_arms_up_frames
 		else:
-			$human_spr.frames = male_arms_down_frames
+			human_spr.frames = male_arms_down_frames
 	if curr_gender == Gender.FEMALE:
 		if carrying_item == true:
-			$human_spr.frames = female_arms_up_frames
+			human_spr.frames = female_arms_up_frames
 		else:
-			$human_spr.frames = female_arms_down_frames
+			human_spr.frames = female_arms_down_frames
 
 
 func _on_switch_gender_pressed():
@@ -113,10 +115,12 @@ func _on_switch_gender_pressed():
 		print(curr_gender)
 	_update_spritesheet()
 
+
 func _on_switch_stance_pressed():
 	carrying_item = !carrying_item
 	print("Carrying item: ", carrying_item)
 	_update_spritesheet()	
+
 
 func _input(event):
 	if event.is_action_pressed("pickupthrow"):
@@ -126,6 +130,7 @@ func _input(event):
 			if !items_in_range.is_empty():
 				_pickup_item(items_in_range.pick_random())
 
+
 func _pickup_item(item: Area2D):
 	item.queue_free()
 	carrying_item = true
@@ -133,11 +138,11 @@ func _pickup_item(item: Area2D):
 	_update_spritesheet()	
 	
 	item_spr.show()
-	
+
+
 func drop_item():
 	item_spr.hide()
 	var item = item_drop.instantiate()
-	var item_sprite = item.get_node("item_spr")
 	
 	# Calculate the drop position in front of the character based on direction
 	var drop_offset = dir * 20 # Multiply direction by a distance factor (e.g., 12)
@@ -152,10 +157,12 @@ func drop_item():
 	print("Carrying item: ", carrying_item)
 	_update_spritesheet()	
 
+
 func _on_pickup_range_area_entered(area: Area2D):
 	if area.is_in_group("item_drop"):
 		items_in_range.append(area)
 		print(items_in_range)
+
 
 func _on_pickup_range_area_exited(area: Area2D):
 	if area.is_in_group("item_drop"):
