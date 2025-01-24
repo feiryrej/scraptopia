e30f1e3ca9c1637816e1132049bad8d10995813f
Hans Queja <hansqueja8@gmail.com>
2025-01-24 10:15:48 +0800

    feat(settings): added small features

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,6 +1,12 @@
 extends Control
 
+# variables
+@onready var settings = load("res://scenes/settings_panel.tscn")
+
+@onready var settings_instance = settings.instantiate()
+
 func _ready():
+	add_child(settings_instance)
 	visible = false
 	$AnimationPlayer.play("RESET")
 
@@ -11,11 +17,12 @@ func resume():
 	$AnimationPlayer.play("RESET")
 
 func pause():
+	settings_instance.visible = false
 	get_tree().paused = true
 	visible = true
 	$AnimationPlayer.stop() 
 	$AnimationPlayer.play("blur")
-
+	
 func _input(event):
 	if event.is_action_pressed("escape"):
 		if get_tree().paused:
@@ -29,11 +36,12 @@ func _on_resume_pressed():
 func _on_restart_pressed():
 	get_tree().paused = false
 	visible = false
-	$AnimationPlayer.stop()
-	$AnimationPlayer.play("transition")
-	$AnimationPlayer.play("RESET")
 	await get_tree().process_frame
 	get_tree().reload_current_scene()
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_settings_pressed() -> void:
+	settings_instance.visible = true
+	print("settings pressed")
