b9ba5b17d6d459c48971135612dffe4dc9b0370f
Hans Queja <hansqueja8@gmail.com>
2025-01-26 17:45:18 +0800

    feat(almanac): add almanac UI and script acting as user guide

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,22 +2,25 @@ extends Control
 
 # variables
 @onready var settings = load("res://scenes/settings_panel.tscn")
-
+@onready var almanac = load("res://scenes/almanac.tscn")
 @onready var settings_instance = settings.instantiate()
+@onready var almanac_instance = almanac.instantiate()
 
 func _ready():
 	add_child(settings_instance)
+	add_child(almanac_instance)
 	visible = false
 	$AnimationPlayer.play("RESET")
 
 func resume():
 	get_tree().paused = false
 	visible = false
-	print("IT DEBLURS")
+	almanac_instance.visible = false
 	$AnimationPlayer.play("RESET")
 
 func pause():
 	settings_instance.visible = false
+	almanac_instance.visible = false
 	get_tree().paused = true
 	visible = true
 	$AnimationPlayer.stop() 
@@ -29,6 +32,7 @@ func _input(event):
 			resume()
 		else:
 			pause()
+			
 
 func _on_resume_pressed():
 	resume()
@@ -45,3 +49,8 @@ func _on_quit_pressed():
 func _on_settings_pressed() -> void:
 	settings_instance.visible = true
 	print("settings pressed")
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if visible:
+		almanac_instance.visible = false
